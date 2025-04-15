380047300c8fe395c3319ba127d015f913f1132b
Damien Thompson <sedativo@gmail.com>
2025-04-14 23:35:30 -0400

    feat: extend map controls store

---
+++ components/map/MapThemeToggleButton.tsx
@@ -0,0 +1,22 @@
+import MapControlContainer from './MapControlContainer';
+import IconButton from '../ui/IconButton';
+
+import { useIsLightMode, useMapActions } from '~/stores/mapControlsStore';
+
+export default function MapThemeToggleButton() {
+  const isLightMode = useIsLightMode();
+  const { toggleLightMode } = useMapActions();
+
+  const onToggleLightMode = () => toggleLightMode(!isLightMode);
+
+  return (
+    <MapControlContainer>
+      <IconButton
+        icon={isLightMode ? 'moon-sharp' : 'sunny-sharp'}
+        color="white"
+        size={28}
+        onPress={onToggleLightMode}
+      />
+    </MapControlContainer>
+  );
+}
