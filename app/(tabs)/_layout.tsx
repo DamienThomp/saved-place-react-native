2c01a8e0a2838baa03020347b49f8f6c9ef9266c
Damien Thompson <sedativo@gmail.com>
2025-05-12 21:13:51 -0400

    feat: add blurview to bottom tab bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,24 @@
 import { Ionicons } from '@expo/vector-icons';
+import { BlurView } from 'expo-blur';
 import { Tabs } from 'expo-router';
+import { StyleSheet } from 'react-native';
 
 export default function RoutLayout() {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarStyle: { position: 'absolute' },
+        tabBarBackground: () => (
+          <BlurView
+            intensity={80}
+            style={{
+              ...StyleSheet.absoluteFillObject,
+              overflow: 'hidden',
+              backgroundColor: 'transparent',
+            }}
+          />
+        ),
+      }}>
       <Tabs.Screen
         name="(main)"
         options={{
