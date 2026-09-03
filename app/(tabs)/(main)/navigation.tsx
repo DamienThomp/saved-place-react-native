2d3f2669abc7f229a6c5264f8c8fabc73d47fd57
Damien Thompson <sedativo@gmail.com>
2026-09-03 12:27:27 -0400

    fix: back button navigation screen

--- app/(tabs)/(main)/navigation.tsx
+++ app/(tabs)/(main)/navigation.tsx
@@ -1,13 +1,23 @@
-import { Stack } from 'expo-router';
-import { StyleSheet, Text, View } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+import { Alert, StyleSheet, Text, View } from 'react-native';
 
 import LoadingState from '~/components/common/LoadingState';
 import { useNavigationViewModel } from '~/hooks/useNavigationViewModel';
 import NavigationModuleView from '~/modules/navigation-module/src/NavigationModuleView';
 
 export default function NavigationScreen() {
+  const router = useRouter();
   const { status, state } = useNavigationViewModel();
   const { isLoading, error } = status;
+
+  const handleCancel = () => {
+    router.back();
+  };
+
+  const handleArrived = () => {
+    // TODO: Improve arrive at destination handler
+    Alert.alert('Arrived');
+  };
 
   return (
     <>
@@ -15,6 +25,8 @@ export default function NavigationScreen() {
       <LoadingState isLoading={isLoading} error={error}>
         {state && (
           <NavigationModuleView
+            onCancel={handleCancel}
+            onArrived={handleArrived}
             style={{ flex: 1 }}
             mode={state.mode}
             origin={state.origin}
